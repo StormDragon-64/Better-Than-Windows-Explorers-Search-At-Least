e8535fd15bd4642eae388e2cad930de0099a3722
StormDragon_64 <80718549+StormDragon-64@users.noreply.github.com>
2024-05-10 23:03:54 -0700

    Add files via upload

---
+++ autoremove.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "searchDirectory=Enter the path of the directory to search: "
+set "filePath=Enter the path of the .txt file: "
+set "searchText="
+set "deleteFiles="
+
+if not exist "%filePath%" (
+    echo File "%filePath%" not found.
+    exit /b
+)
+
+echo Searching for files with names containing text from "%filePath%" in directory: %searchDirectory%
+
+for /f "tokens=*" %%a in (%filePath%) do (
+    set "fileName=%%a"
+    for /f "delims=" %%b in ('dir /b /a-d "%searchDirectory%\*!fileName!*" 2^>nul') do (
+        echo Found: %%b
+        set "deleteFiles=!deleteFiles! "%%b""
+    )
+)
+
+if not "%deleteFiles%"=="" (
+    set /p confirm="Do you want to delete all found files? (y/n): "
+    if /i "!confirm!"=="y" (
+        for %%f in (%deleteFiles%) do (
+            del "%searchDirectory%\%%~f"
+            echo Deleted: %%~f
+        )
+    )
+)
+
+echo Done.
+pause
